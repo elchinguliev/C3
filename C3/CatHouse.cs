89436c31b5d04668147c951265cd3e3eb91c516b
ASUS <ASUS@DESKTOP-O86GR9L>
2023-01-14 01:25:49 +0400

    Add project files.

---
+++ C3/CatHouse.cs
@@ -0,0 +1,31 @@
+﻿using Homework;
+
+namespace Homework
+{
+    internal class CatHouse
+    {
+        public string Name { get; set; }
+        public List<Cat> Cats { get; set; } = new();
+        public int CatCount { get => Cats.Count; }
+
+        public void AddCat(Cat cat)
+        {
+            foreach (var item in Cats)
+            {
+                if (item.Nickname == cat.Nickname)
+                    throw new Exception("Bele pishik var");
+            }
+            Cats.Add(cat);
+        }
+
+        public void RemoveByNickname(string nickname)
+        {
+            foreach (var item in Cats)
+            {
+                if (item.Nickname == nickname)
+                    Cats.Remove(item);
+            }
+        }
+
+    }
+}
---
+++ C3/PetShop.cs
@@ -0,0 +1,17 @@
+﻿using Homework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal class PetShop
+    {
+        public List<CatHouse> CatHouses { get; set; } = new List<CatHouse>();
+        public int CatHouseCount { get => CatHouses.Count; }
+    }
+}
+
+
